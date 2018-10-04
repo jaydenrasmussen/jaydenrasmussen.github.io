8e479def0319d64aef3ff4dae3978e7cd3f60245
Jayden Rasmussen <jayden.rasmussen@me.com>
2018-10-03 22:55:44 -0600

    inital changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -49,13 +49,13 @@
 
 <script>
 export default {
-  data() {
-    return {
-      items: [
-        { title: 'Home', icon: 'home', to: { name: 'index' } },
-        { title: 'Inspire', icon: 'lightbulb', to: { name: 'inspire' } }
-      ]
+    data() {
+        return {
+            items: [
+                { title: 'Home', icon: 'home', to: { name: 'index' } },
+                { title: 'Inspire', icon: 'lightbulb', to: { name: 'inspire' } }
+            ]
+        }
     }
-  }
 }
 </script>
